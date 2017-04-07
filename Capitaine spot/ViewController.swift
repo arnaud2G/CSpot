583062b8029078e1d9dd8a0101d79b6c203ae74a
arnaud2G <arnaud.verrier@gmail.com>
2017-04-07 10:51:41 +0200

    Initial Commit

---
+++ Capitaine spot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Capitaine spot
+//
+//  Created by 2Gather Arnaud Verrier on 07/04/2017.
+//  Copyright © 2017 Arnaud Verrier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
